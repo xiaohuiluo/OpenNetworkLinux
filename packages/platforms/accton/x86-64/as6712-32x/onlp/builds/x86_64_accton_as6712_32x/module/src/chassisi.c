0c4b934d919790b85479cbdf08d6701df8c0052f
Yi Tseng <yi@opennetworking.org>
2020-02-24 13:24:13 -0800

    Ass chassis API to as7716 sc/xc and as6712

---
+++ packages/platforms/accton/x86-64/as6712-32x/onlp/builds/x86_64_accton_as6712_32x/module/src/chassisi.c
@@ -0,0 +1,42 @@
+#include <onlp/platformi/chassisi.h>
+
+int
+onlp_chassisi_hdr_get(onlp_oid_id_t id, onlp_oid_hdr_t* hdr)
+{
+    int i;
+    onlp_oid_t* e = hdr->coids;
+
+    ONLP_OID_STATUS_FLAG_SET(hdr, PRESENT);
+    ONLP_OID_STATUS_FLAG_SET(hdr, OPERATIONAL);
+
+    /* 5 Thermal sensors on the chassis */
+    for (i = 1; i <= 5; i++)
+    {
+        *e++ = ONLP_THERMAL_ID_CREATE(i);
+    }
+
+    /* 10 LEDs on the chassis */
+    for (i = 1; i <= 10; i++)
+    {
+        *e++ = ONLP_LED_ID_CREATE(i);
+    }
+
+    /* 5 Fans on the chassis */
+    for (i = 1; i <= 5; i++)
+    {
+        *e++ = ONLP_FAN_ID_CREATE(i);
+    }
+
+    /* 2 PSUs on the chassis */
+    for (i = 1; i <= 2; i++)
+    {
+        *e++ = ONLP_PSU_ID_CREATE(i);
+    }
+
+    /* 32 SFPs */
+    for(i = 1; i <= 32; i++) {
+        *e++ = ONLP_SFP_ID_CREATE(i);
+    }
+
+    return 0;
+}
